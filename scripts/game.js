8c842550d47443538143e33ae5815ca98e37a3a3
El Brewster <brewsterel3@gmail.com>
2022-11-12 20:15:53 -0800

    Add game class methods

--- scripts/game.js
+++ scripts/game.js
@@ -2,30 +2,75 @@
 class Game {
     constructor() {
         this.boardPositions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        this.player1 = "X";
-        this.player2 = "O";
+        // this.playerX = playerObj1;
+        // this.playerO = playerObj2;
         this.gameNo = 0;
     } 
 
-    startNewGame() {
+    startNewGame(playerObj1, playerObj2) {
         var gamerThem = new Player(them, theirToken);
         var gamerOther = new Player(other, otherToken);
-        this.
-    }
+        this.setPlayerTurn();
+        this.gameNo++;
+    } 
+
     setPlayerTurn(player1, player2) {
-        //switch turn -->this probably goes in our Game class?
-            // do I instantiate two instances of gamers here? or in the Game class somewhere?
         if(player1.tokenPlacement === false) {
-            //run squarePicker, in squarePicker switch to "true"
+            player1.squarePicker();
             player1.tokenPlacement = true;
             player2.tokenPlacement = false;
         }
         if(player2.tokenPlacement === false) {
-            //run squarePicker, in squarePicker switch to "true"
+            player2.squarePicker();
             player2.tokenPlacement = true;
             player1.tokenPlacement = false;
         }
         console.log(player1, player2);
+    }
+// need some way to have this ^^ run every time there is tokenPlacement/buttonclick of our numpad
+    squarePicker(click) {
+        var player;
+        for (var i = 0; i < this.boardPositions.length; i++) {
+            if (click === this.boardPositions[i]) {
+                this.boardPositions.splice(i, 1);
+                player.chosenSpots.push(click); 
+                console.log(player.chosenSpots);
+                //add in a "disabled" state for the button clicked
+            }
+        }
+    }
+
+    winEval() {
+        var winner = [4, 5, 9, 1]; 
+        // -->probably refer to our player instances to refer to their array
+        winner.sort();
+        var dinner = winner.join("");
+        var dinnerStr = dinner.toString();
+        if(dinnerStr.includes("1") && dinnerStr.includes("2") && dinnerStr.includes("3")) {
+            console.log(dinnerStr);
+        } 
+        if(dinnerStr.includes("1") && dinnerStr.includes("4") && dinnerStr.includes("7")) {
+            console.log(dinnerStr); 
+        }
+        if(dinnerStr.includes("1") && dinnerStr.includes("5") && dinnerStr.includes("9")) {
+            console.log(dinnerStr); 
+        }
+        if(dinnerStr.includes("2") && dinnerStr.includes("5") && dinnerStr.includes("8")) {
+            console.log(dinnerStr); 
+        }
+        if(dinnerStr.includes("3") && dinnerStr.includes("5") && dinnerStr.includes("7")) {
+            console.log(dinnerStr); 
+        }
+        if(dinnerStr.includes("3") && dinnerStr.includes("6") && dinnerStr.includes("9")) {
+            console.log(dinnerStr); 
+        }
+        if(dinnerStr.includes("4") && dinnerStr.includes("5") && dinnerStr.includes("6")) {
+            console.log(dinnerStr); 
+        }
+        if(dinnerStr.includes("7") && dinnerStr.includes("8") && dinnerStr.includes("9")) {
+            console.log(dinnerStr); 
+        }    //   ---> then trigger a win method (banner, timeout, then reset stuff)
+        //     ADD DRAW HERE (like if a player array has 5 or more index positions, declare draw)(is it 5 or 4?)
     }
 }
 
