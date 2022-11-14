0031453d672be36595bd39203a1060d08ff45781
El Brewster <brewsterel3@gmail.com>
2022-11-14 10:00:03 -0800

    Quick save

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-// var player1 = new Player('Rex', 'smelly');
-// var player2 = new Player('Lex', 'stinky');
+// var player1 = new Player("Rex", "X");
+// var player2 = new Player("Lex", "O");
 // var newGame = new Game(player1, player2);
 var player1;
 var player2;
@@ -24,9 +24,11 @@ var btn3 = document.querySelector("#numPad3");
 // --------------------------------------------------------
 startBtn.addEventListener("click", startBtnInit);
 // btnField.addEventListener("click", whatever);
-btn7.addEventListener("click", function cosmeticClick() {
-    btn7.textContent = player.icon;
-});
+btn7.addEventListener("click", cosmeticClick);
+function cosmeticClick(player1) {
+    alert("hi El");
+    btn7.textContent = player1.token;
+}
 btn8.addEventListener("click", whatever);
 btn9.addEventListener("click", whatever);
 btn4.addEventListener("click", whatever);
@@ -38,39 +40,27 @@ btn3.addEventListener("click", whatever);
 // --------------------------------------------------------
 
 function startBtnInit() {
-    //buttons 1-9 activate
     //this starts my game
-    player1 = new Player('Rex', "&#128566");
-    player2 = new Player('Lex', "&#128565");
+    //buttons 1-9 activate
+    player1 = new Player("Rex", "X");
+    // "&#128566"
+    player2 = new Player("Lex", "O");
+    // "&#128565"
     newGame = new Game(player1, player2);
     // newGame.gameNo++;
-    // newGame.setPlayerTurn(player1, player2);
     //banner announcement
     topBanner.innerText = "Let's goooooo!";
+    //we need a pause here somehow before player turn is announced
+    topBanner.innerText = `${player1} picks and clicks a spot now.`
 
     console.log(player1, player2, newGame);
 
 }
 
-function whatever() {
-    //how do I attach specific variable to these eventListeners? or what even am I doing here?
-    console.log(newGame);
-} 
-//something that runs squarePicker so that it loops through turn switches until positions filled at 9
-
-function bannerUpdate () {
-    //this is more for record-keeping
-    topBanner.innerText = "Let's goooooo!"
-    topBanner.innerText = `${player1} picks and clicks a spot now.`
-    topBanner.innerText = "Game Over: Nobody Won!"
-    topBanner.innerText = `Hey yea! ${player1} wins this one!`
-    //on second draw: "Maybe bribe your friend with sweets to let you win!" and so on
-    //"What if you took turns planning to win?"
-    //"Tic tac toe has garbage gameplay design, learn social engineering to get the results you want"
-    //"I bet you wish you were playing chess!"
-    //"Take a sharpie and draw a 'cat's tail' on this thing!"
-    console.log(bannerstuff);
-}
+// function turnStart(player1, player2) {
+//     topBanner.innerText = `${player1} picks and clicks a spot now.`;
+//     newGame.setPlayerTurn(player1, player2);
+// }
 
 function setPlayerTurn(player1, player2) {
     var spotNumber;
@@ -104,8 +94,26 @@ function otherGameResetFunction() {
     console.log(timeout);
 }
 
+function bannerUpdate () {
+    //this is more for record-keeping
+    topBanner.innerText = "Let's goooooo!"
+    topBanner.innerText = `${player1} picks and clicks a spot now.`
+    topBanner.innerText = "Game Over: Nobody Won!"
+    topBanner.innerText = `Hey yea! ${player1} wins this one!`
+    //on second draw: "Maybe bribe your friend with sweets to let you win!" and so on
+    //"What if you took turns planning to win?"
+    //"Tic tac toe has garbage gameplay design, learn social engineering to get the results you want"
+    //"I bet you wish you were playing chess!"
+    //"Take a sharpie and draw a 'cat's tail' on this thing!"
+    console.log(bannerstuff);
+}
 //HTML emoji stuf: lightning 9889 star11088 diamond 128142 explosion128165
 //explosion speech bubble 128495 siren 128680
 //point left arrow
 //128566 face without mouth
 //128565	 face with x eyes
+function whatever() {
+    // alert("hi El");
+    //how do I attach specific variable to these eventListeners? or what even am I doing here?
+    console.log(newGame);
+} 
